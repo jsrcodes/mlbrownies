57dde88ade7c8a8b20218da68ce341a87dff67e3
jsrcodes <jsrcodes@users.noreply.github.com>
2021-03-11 17:14:51 +0000

    deploy: 0b74664c5534c70e0b3d5293ccec84900092d031

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/mlidiot/assets/js/search-data.json', true);
+    request.open('GET', '/mlbrownies/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
